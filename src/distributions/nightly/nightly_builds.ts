f03f2c964e6f70271de2c0d8237aca7d7690ab31
ajamjambrut <ajamjambrut@gmail.com>
2025-07-12 02:38:08 +0700

    Update nightly_builds.ts

--- src/distributions/nightly/nightly_builds.ts
+++ src/distributions/nightly/nightly_builds.ts
@@ -1,14 +1,1 @@
-import BasePrereleaseNodejs from '../base-distribution-prerelease';
-import {NodeInputs} from '../base-models';
 
-export default class NightlyNodejs extends BasePrereleaseNodejs {
-  protected distribution = 'nightly';
-  constructor(nodeInfo: NodeInputs) {
-    super(nodeInfo);
-  }
-
-  protected getDistributionUrl(mirror: string): string {
-    const url = mirror || 'https://nodejs.org';
-    return `${url}/download/nightly`;
-  }
-}
